e2d02d3a4a2bf8f2216e8848fb0adc163f09be0b
Carlos Zapata <carloszapata@Carloss-MBP.cable.rcn.com>
2022-06-29 22:52:04 -0500

    Configure React App for Deployment

---
+++ build/precache-manifest.793764eccfc99ebb858da7feec5c2b82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a43acb1986b965c4b9f0",
+    "url": "/formikForm/static/css/main.09c9785a.chunk.css"
+  },
+  {
+    "revision": "a43acb1986b965c4b9f0",
+    "url": "/formikForm/static/js/main.a43acb19.chunk.js"
+  },
+  {
+    "revision": "ea2a47724b4b1ac2e4eb",
+    "url": "/formikForm/static/js/1.ea2a4772.chunk.js"
+  },
+  {
+    "revision": "e8aa4a20fd9b6d8aa4ef",
+    "url": "/formikForm/static/js/runtime~main.e8aa4a20.js"
+  },
+  {
+    "revision": "8a1bdc189c0fb131dac77171c46d8130",
+    "url": "/formikForm/index.html"
+  }
+];
